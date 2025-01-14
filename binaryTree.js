5c3791723ad8761bb38cb6e07af78b6f1b031b96
moises valdez <moisesvaldez0559@gmail.com>
2025-01-14 11:58:46 -0800

    learning binary trees

---
+++ binaryTree.js
@@ -0,0 +1,67 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (currentNode.left === null) {
+            currentNode.left = newNode;
+            break;
+          } else {
+            currentNode = currentNode.left;
+          }
+        } else {
+          if (currentNode.right === null) {
+            currentNode.right = newNode;
+            break;
+          } else {
+            currentNode = currentNode.right;
+          }
+        }
+      }
+    }
+  }
+
+  lookup(value) {}
+
+  //remove(){}
+}
+
+const tree = new BinarySearchTree();
+
+//   9
+// 4   20
+//1 6 25 170
+
+tree.insert(9);
+tree.insert(4);
+// tree.insert(5);
+// tree.insert(1);
+// tree.insert(20);
+// tree.insert(170);
+tree.insert(15);
+
+console.log(JSON.stringify(traverse(tree.root)));
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
