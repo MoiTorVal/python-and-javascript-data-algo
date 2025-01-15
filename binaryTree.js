ad26731b8e740bce3070b4116948e736604ad7c1
moises valdez <moisesvaldez0559@gmail.com>
2025-01-15 11:56:40 -0800

    add lookup to my bst

--- binaryTree.js
+++ binaryTree.js
@@ -38,7 +38,23 @@ class BinarySearchTree {
     }
   }
 
-  lookup(value) {}
+  lookup(value) {
+    if (this.root === null) {
+      return null;
+    } else {
+      let currentNode = this.root;
+      do {
+        if (currentNode.value === value) {
+          return currentNode;
+        } else if (currentNode.value > value) {
+          currentNode = currentNode.left;
+        } else if (currentNode.value < value) {
+          currentNode = currentNode.right;
+        }
+      } while (currentNode !== null);
+      return null;
+    }
+  }
 
   //remove(){}
 }
@@ -46,18 +62,18 @@ class BinarySearchTree {
 const tree = new BinarySearchTree();
 
 //   9
-// 4   20
+// 4   10
 //1 6 25 170
 
 tree.insert(9);
 tree.insert(4);
-// tree.insert(5);
-// tree.insert(1);
-// tree.insert(20);
-// tree.insert(170);
-tree.insert(15);
+tree.insert(5);
+tree.insert(1);
+tree.insert(20);
+tree.insert(170);
+tree.insert(10);
 
-console.log(JSON.stringify(traverse(tree.root)));
+console.log(tree.lookup(4));
 
 function traverse(node) {
   const tree = { value: node.value };
